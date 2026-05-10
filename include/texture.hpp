4c3f09cf2e12e1afcba201fe48670b78cce9f584
spatchler <icodecubes@outlook.com>
2026-05-10 21:00:27 +0100

    Fixed texture sampling

--- include/texture.hpp
+++ include/texture.hpp
@@ -2,22 +2,30 @@
 
 #include <stb_image.h>
 
+#include <glm/glm.hpp>
+
 #include <string>
+#include <algorithm>
+#include <print>
 
 namespace VMesh {
   class Texture {
   public:
     Texture(const std::string& pPath);
+    Texture(const glm::vec3& pColour);
 
     glm::vec3 sample(const glm::vec2& pTexCoord);
 
     const std::string& getPath();
 
-    ~Texture();
+    void release();
   private:
-    const std::string mPath;
+    bool mIsColour;
+    glm::vec3 mColour;
+    std::string mPath;
     uint8_t* mData;
-    glm::vec2 mSize;
+    glm::ivec2 mSize;
+    uint mArea;
     int mNrComponents;
   };
 }
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,24 +1,37 @@
+#include "texture.hpp"
+
 using namespace VMesh;
 
 Texture::Texture(const std::string& pPath)
-:mData = stbi_load(pPath.c_str(), &mSize.x, &mSize.y, &mNrComponents, 3), mPath(pPath) {
+:mPath(pPath), mIsColour(false) {
+  std::replace(mPath.begin(), mPath.end(), '\\', '/');
+  mData = stbi_load(mPath.c_str(), &mSize.x, &mSize.y, &mNrComponents, 3); // Force 3 channels
+  mArea = mSize.x * mSize.y;
   if (!mData) {
-    std::println("Texture failed to load at path: {0}", pPath);
+    std::println("Texture failed to load at path: {0}", mPath);
     stbi_image_free(mData);
   }
 }
 
+Texture::Texture(const glm::vec3& pColour)
+:mIsColour(true), mColour(pColour) {
+}
+
 glm::vec3 Texture::sample(const glm::vec2& pTexCoord) {
-  glm::ivec2 pixelCoord = pTexCoord * mSize;
-  uint i = (pixelCoord.y / mSize.y * mSize.y + pixelCoord.x) * 3;
-  uint8_t r = mData[i], g = mData[++i], b = mData[++i];
-  return glm::vec3(r, g, b) / 255;
+  if (mIsColour) return mColour;
+  // std::println("sampling");
+  glm::ivec2 pixelCoord = glm::ivec2(glm::abs(pTexCoord)) * mSize;
+  uint i = ((pixelCoord.y * mSize.y) + pixelCoord.x) * mNrComponents % mArea;
+  uint8_t r = *(mData+i), g = *(mData+i+1), b = *(mData+i+2);
+  // std::println();
+  // uint8_t r = mData[i], g = mData[++i], b = mData[++i];
+  return glm::vec3(r, g, b) / 255.f;
 }
 
 const std::string& Texture::getPath() {
   return mPath;
 }
 
-Texture::~Texture() {
-  stbi_image_free(mData);
+void Texture::release() {
+  if (!mIsColour) stbi_image_free(mData);
 }
